cd21be220c822e8f3dd4ccab95c53f711017b542
margo <margo.malashko@gmail.com>
2025-02-25 02:05:04 +0300

    Task-1: Implemented rotation

---
+++ src/main/java/ru/alexnsk/starwars/rotation/Rotatable.java
@@ -0,0 +1,7 @@
+package ru.alexnsk.starwars.rotation;
+
+public interface Rotatable {
+
+    int getAngle();
+    void setAngle(int angle);
+}
---
+++ src/main/java/ru/alexnsk/starwars/rotation/RotationExecutor.java
@@ -0,0 +1,20 @@
+package ru.alexnsk.starwars.rotation;
+
+public class RotationExecutor {
+
+    private static final int FULL_CIRCLE = 360;
+
+    private final Rotatable rotatable;
+
+    public RotationExecutor(Rotatable rotatable) {
+        this.rotatable = rotatable;
+    }
+
+    public void execute(int angle) {
+        int newAngle = (rotatable.getAngle() + angle) % FULL_CIRCLE;
+        if (newAngle < 0) {
+            newAngle += FULL_CIRCLE;
+        }
+        rotatable.setAngle(newAngle);
+    }
+}
